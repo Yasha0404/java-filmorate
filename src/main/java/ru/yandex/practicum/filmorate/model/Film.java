0ba63281c7aaafb5f2fe3890d19ee1aebda2412a
Яшар Исмаилов <yromancov@gmail.com>
2026-07-26 10:24:08 -0400

    Ðнаписать тесты

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,10 +20,11 @@ import java.util.Date;
 @EqualsAndHashCode(of = {"id"})
 public class Film {
     private Long id;
+    @NotBlank
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Duration duration;
+    private int duration;
 
 
 }
