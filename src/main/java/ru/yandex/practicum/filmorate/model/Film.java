0e067c5c2527346193efacc5ea019c15dce222d0
Яшар Исмаилов <yromancov@gmail.com>
2026-07-26 06:37:02 -0400

    Добавить Ðмодель и эндÐпонинт

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * Film.
  */
 @Getter
 @Setter
+@EqualsAndHashCode(of = {"id"})
 public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
+
+
 }
