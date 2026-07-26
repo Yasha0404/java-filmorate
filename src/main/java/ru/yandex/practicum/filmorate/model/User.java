776df471547e8d03609f032d83db4591a4a6b2e3
Яшар Исмаилов <yromancov@gmail.com>
2026-07-26 09:33:57 -0400

    Закончить реализацуию сузноÑщностей

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 @Data
+@EqualsAndHashCode(of = "id")
 public class User {
     private Long id;
     private String email;
